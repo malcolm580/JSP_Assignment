ecab3659b4231fa1267fa5ef79ad37cf41e84567
On Chun Arthur LIU <160322449@stu.vtc.edu.hk>
2017-11-20 16:44:41 +0800

    Login Added

--- JSP.sql
+++ JSP.sql
@@ -1,10 +1,47 @@
-CREATE TABLE `User` (UserID int(10) NOT NULL AUTO_INCREMENT, Username varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL, Role varchar(255) NOT NULL, Email varchar(255), PRIMARY KEY (UserID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE Module (ModuleID int(10) NOT NULL AUTO_INCREMENT, ModuleName varchar(255) NOT NULL, PRIMARY KEY (ModuleID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE Metrial (ModuleID int(10) NOT NULL, ContentType varchar(255) NOT NULL, Content varchar(255), BlackListId_JSON varchar(255) NOT NULL, PRIMARY KEY (ModuleID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE Quiz (QuizID int(10) NOT NULL AUTO_INCREMENT, ModuleID int(10) NOT NULL, TimeLimit int(10), QuizName varchar(255) NOT NULL, PRIMARY KEY (QuizID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE Question (QuizID int(10) NOT NULL, QuestionID int(10) NOT NULL AUTO_INCREMENT, QuestionType varchar(255) NOT NULL, Question varchar(255) NOT NULL, CorrectAnswer varchar(255), Options_JSON varchar(255), PRIMARY KEY (QuestionID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE QuizResult (UserID int(10), CourseID int(10), QuizID int(10) NOT NULL, QuizResultID int(10) NOT NULL AUTO_INCREMENT, Duration int(10), AnsweringQuestionState_JSON int(10), PRIMARY KEY (QuizResultID)) ENGINE=InnoDB CHARACTER SET UTF8;
-CREATE TABLE UserModule (UserID int(10) NOT NULL, ModuleID int(10) NOT NULL) ENGINE=InnoDB CHARACTER SET UTF8;
+CREATE TABLE `User` (
+  UserID   int(10) NOT NULL AUTO_INCREMENT, 
+  Username varchar(255) NOT NULL UNIQUE, 
+  Password varchar(255) NOT NULL, 
+  Role     varchar(255) NOT NULL, 
+  Email    varchar(255), 
+  PRIMARY KEY (UserID)) type=InnoDB;
+CREATE TABLE Module (
+  ModuleID   int(10) NOT NULL AUTO_INCREMENT, 
+  ModuleName varchar(255) NOT NULL, 
+  PRIMARY KEY (ModuleID)) type=InnoDB;
+CREATE TABLE Metrial (
+  ModuleID         int(10) NOT NULL, 
+  ContentType      varchar(255) NOT NULL, 
+  Content          varchar(255), 
+  BlackListId_JSON varchar(255) NOT NULL, 
+  PRIMARY KEY (ModuleID)) type=InnoDB;
+CREATE TABLE Quiz (
+  QuizID    int(10) NOT NULL AUTO_INCREMENT, 
+  ModuleID  int(10) NOT NULL, 
+  TimeLimit int(10), 
+  QuizName  varchar(255) NOT NULL, 
+  PRIMARY KEY (QuizID)) type=InnoDB;
+CREATE TABLE Question (
+  QuizID        int(10) NOT NULL, 
+  QuestionID    int(10) NOT NULL AUTO_INCREMENT, 
+  QuestionType  varchar(255) NOT NULL, 
+  Question      varchar(255) NOT NULL, 
+  CorrectAnswer varchar(255), 
+  Options_JSON  varchar(255), 
+  PRIMARY KEY (QuestionID)) type=InnoDB;
+CREATE TABLE QuizResult (
+  UserID                      int(10), 
+  CourseID                    int(10), 
+  QuizID                      int(10) NOT NULL, 
+  QuizResultID                int(10) NOT NULL AUTO_INCREMENT, 
+  Duration                    int(10), 
+  AnsweringQuestionState_JSON int(10), 
+  PRIMARY KEY (QuizResultID)) type=InnoDB;
+CREATE TABLE UserModule (
+  UserID   int(10) NOT NULL UNIQUE, 
+  ModuleID int(10) NOT NULL UNIQUE, 
+  PRIMARY KEY (UserID, 
+  ModuleID)) type=InnoDB;
 ALTER TABLE Metrial ADD INDEX FKMetrial663747 (ModuleID), ADD CONSTRAINT FKMetrial663747 FOREIGN KEY (ModuleID) REFERENCES Module (ModuleID);
 ALTER TABLE Quiz ADD INDEX FKQuiz354528 (ModuleID), ADD CONSTRAINT FKQuiz354528 FOREIGN KEY (ModuleID) REFERENCES Module (ModuleID);
 ALTER TABLE Question ADD INDEX FKQuestion143290 (QuizID), ADD CONSTRAINT FKQuestion143290 FOREIGN KEY (QuizID) REFERENCES Quiz (QuizID);
