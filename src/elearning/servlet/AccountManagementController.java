cbc8f49324275b1d64ae7b0c662d3cba947aca26
malcolm580 <youmalcolm580@gmail.com>
2017-11-26 17:14:40 +0800

    ....

--- src/elearning/servlet/AccountManagementController.java
+++ src/elearning/servlet/AccountManagementController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 
@@ -45,6 +46,8 @@ public class AccountManagementController extends HttpServlet {
 
             targetURL = "AccountManagement/UserList.jsp";
 
+
+
         }else if("view".equalsIgnoreCase(action)){
 
             String userID = request.getParameter("userID");
@@ -58,14 +61,17 @@ public class AccountManagementController extends HttpServlet {
 
             targetURL = "AccountManagement/ViewSpecificUser.jsp";
 
+
+
         }else if("edit".equalsIgnoreCase(action)){
 
             String userID = request.getParameter("userID");
-            String userName = request.getParameter("userName");
+            String userName = request.getParameter("username");
             String password = request.getParameter("password");
             String role = request.getParameter("role");
             String email = request.getParameter("email");
             Boolean edited = false;
+
 
             try{
                 edited = db.editUserInfoAndRole(Integer.parseInt(userID), userName , password , role , email);
@@ -80,9 +86,11 @@ public class AccountManagementController extends HttpServlet {
 
         }
 
+
         RequestDispatcher rd;
         rd = getServletContext().getRequestDispatcher("/" + targetURL);
         rd.forward(request, response);
+
     }
 
 
